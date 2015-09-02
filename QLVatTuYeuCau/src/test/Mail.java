6e40b65e16258c08938e1838873e5c149a505039
camtien <camtien.le1994@gmail.com>
2015-09-02 18:55:55 +0700

    Merge branch 'master' of https://github.com/free-development/QuanLyVatTu
    Conflicts:
    	QLVatTuYeuCau/WebContent/yeu-cau-vat-tu.jsp
    	QLVatTuYeuCau/src/VatTu.hbm.xml

---
+++ QLVatTuYeuCau/src/test/Mail.java
@@ -0,0 +1,53 @@
+package test;
+
+//import java.util.Properties;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Mail {
+	public static void main(String[] args) {
+		
+	  
+	 String to="quoipro94@gmail.com";//change accordingly  
+	  
+	  //Get the session object  
+	  Properties props = new Properties(); 
+	  props.put("mail.smtp.host", "smtp.gmail.com");  
+	  props.put("mail.smtp.socketFactory.port", "465");  
+	  props.put("mail.smtp.socketFactory.class",  
+	            "javax.net.ssl.SSLSocketFactory");  
+	  props.put("mail.smtp.auth", "true");  
+	  props.put("mail.smtp.port", "465");  
+	   
+	  Session session = Session.getDefaultInstance(props,  
+	   new javax.mail.Authenticator() {  
+	   protected PasswordAuthentication getPasswordAuthentication() {  
+	   return new PasswordAuthentication("evnCanTho@gmail.com","evnCanTho2015");//change accordingly  
+	   }  
+	  });  
+	   
+	  //compose message  
+	  try {  
+	   MimeMessage message = new MimeMessage(session);  
+	   message.setFrom(new InternetAddress("evnCanTho@gmail.com"));//change accordingly  
+	   message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
+	   message.setSubject("Hello");  
+	   message.setText("Testing.......");  
+	     
+	   //send message  
+	   Transport.send(message);  
+	  
+	   System.out.println("message sent successfully");  
+	   
+	  } catch (MessagingException e) {throw new RuntimeException(e);}  
+	   
+	 }  
+}
