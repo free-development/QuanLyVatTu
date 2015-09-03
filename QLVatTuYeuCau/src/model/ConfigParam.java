7e25c944866a9baf7b3bb71b51cd474f266cb59b
camtien <camtien.le1994@gmail.com>
2015-09-03 18:39:03 +0700

    update

---
+++ QLVatTuYeuCau/src/model/ConfigParam.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author quoioln
+ *
+ */
+public class ConfigParam {
+	public static final String truongPhongMa = "TP";
+	public static final String adminMa = "AD";
+	public static final String vanThuMa = "VT";
+}
